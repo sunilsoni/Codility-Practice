5c094c08f97befe7e7edc48b67d87bec48e6ddba
sunilsoni <sunilsoni@live.in>
2017-09-17 17:37:33 +0530

    first commit

--- src/com/codility/lessons/Arrays/OddOccurrencesInArray.java
+++ src/com/codility/lessons/Arrays/OddOccurrencesInArray.java
@@ -2,9 +2,23 @@ package com.codility.lessons.Arrays;
 
 public class OddOccurrencesInArray {
 
+	public int solution(int[] A) {
+		int unpaired = 0;
+		for (int number : A) {
+			unpaired ^= number;
+		}
+		return unpaired;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		OddOccurrencesInArray odd= new OddOccurrencesInArray();
+		
+		int[] A = {9,3,9,3,9,7,9};
+		
+		int res = odd.solution(A);
+		System.out.println("res===>"+res);
 
+		//https://codility.com/demo/results/trainingNRQ4RE-4T9/
 	}
 
 }
