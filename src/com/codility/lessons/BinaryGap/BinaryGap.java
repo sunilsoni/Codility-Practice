adf1483901d0411d2a6b9c31fdfe39973a6a6f33
sunilsoni <sunilsoni@live.in>
2017-09-17 11:14:51 +0530

    first commit

--- src/com/codility/lessons/BinaryGap/BinaryGap.java
+++ src/com/codility/lessons/BinaryGap/BinaryGap.java
@@ -1,33 +1,37 @@
 package com.codility.lessons.BinaryGap;
 
 /**
- * A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
+ * <p>A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
 
-For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps.
+<p>For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps.
 
-Write a function:
+<p>Write a function:
 
-class Solution { public int solution(int N); }
+<p>class Solution { public int solution(int N); }
 
-that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
+<p>that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
 
-For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5.
+<p>For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5.
 
-Assume that:
+<p>Assume that:
 
-N is an integer within the range [1..2,147,483,647].
-Complexity:
+<p>N is an integer within the range [1..2,147,483,647].
+<p>Complexity:
 
-expected worst-case time complexity is O(log(N));
-expected worst-case space complexity is O(1).
+<p>expected worst-case time complexity is O(log(N));
+<p>expected worst-case space complexity is O(1).
+
+<p>{@link #Bitwise Operation https://en.wikipedia.org/wiki/Bitwise_operation}
  */
+
 public class BinaryGap {
 	public int solution(int n) { 
 		// get rid of right-hand zeros
 	    while (n != 0 && (n & 1) == 0) {
 	        n >>>= 1;
 	    }
-
+	    System.out.println("n--->"+n);
+	    
 	    int max = 0;
 	    int gap = 0;
 	    while (n != 0) {
