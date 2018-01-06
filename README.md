461eae8203d1f2fb09f19df23053e43b77f93a42
Sunil Soni <sunilsoni@live.in>
2018-01-06 11:59:50 +0530

    readme added

--- README.md
+++
@@ -1,2 +0,0 @@
-# Codility-Practice
-Codility Lesson Practice
--- src/com/codility/lessons/TimeComplexity/TapeEquilibrium.java
+++ src/com/codility/lessons/TimeComplexity/TapeEquilibrium.java
@@ -3,7 +3,7 @@ package com.codility.lessons.TimeComplexity;
 public class TapeEquilibrium {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 
 
 	}
 
