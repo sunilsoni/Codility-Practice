5a02e3c56a970630e8b8a8ce43f7ec98d8ee12df
Sunil Soni <sunilsoni@live.in>
2018-01-07 12:13:56 +0530

    Convert to Spring Boot Project

---
+++ src/main/java/com/codility/CodilityPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.codility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodilityPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodilityPracticeApplication.class, args);
+	}
+}
